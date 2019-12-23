84442cb6657c24455dbe63cf9d354757e6c7c67e
ShantnuS <shansin@microsoft.com>
2019-12-23 17:39:21 +0000

    Copies tabs

---
+++ README.md
@@ -0,0 +1,11 @@
+# CopyTabs
+
+Browser extension that copies open tabs into clipboard
+
+## Contributing
+
+PRs accepted.
+
+## Credits
+
+"<div>Icons made by <a href="https://www.flaticon.com/authors/google" title="Google">Google</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>" 
---
+++ popup.js
@@ -0,0 +1,24 @@
+dechocument.addEventListener('DOMContentLoaded', function () {
+	var btn = document.getElementById("btnCopy");
+    btn.addEventListener('click', copyUrls);
+});
+
+function copyUrls(){
+    var urls = "";
+    chrome.windows.getAll({populate:true},function(windows){
+        windows.forEach(function(window){
+            window.tabs.forEach(function(tab){
+                urls = urls + tab.title + ": " + tab.url + "\n";
+            });
+        });
+        copyToClipboard(urls);
+    });
+}
+
+function copyToClipboard(urls){
+    var copyText = document.getElementById('txtCopy');
+    copyText.value = urls;
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+}
